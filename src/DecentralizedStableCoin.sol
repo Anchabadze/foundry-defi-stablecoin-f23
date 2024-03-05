ed3131bccc8215c3ff5925ea3511dd9687d14eaf
anchabadze <zurab.anchabadze@gmail.com>
2024-03-05 18:14:19 -0300

    first commit

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+// Layout of Contract:
+// version
+// imports
+// interfaces, libraries, contracts
+// errors
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+/*
+ * @title DecentralizedStableCoin
+ * @author Zurab Anchabadze
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting (Stability Mechanism): Decentralized (Algorithmic)
+ * Value (Relative Stability): Anchored (Pegged to USD)
+ * Collateral Type: Crypto
+ *
+ * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20 implementation of our stablecoin system.
+ *
+ */
+
+contract DecentralizedStableCoin {
+
+}
