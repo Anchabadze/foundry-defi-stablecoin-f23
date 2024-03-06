c767ef27ace6e86fc92b7b9978b5c5437abcfa91
anchabadze <zurab.anchabadze@gmail.com>
2024-03-06 17:40:04 -0300

    DSC.sol finished

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -23,6 +23,9 @@ pragma solidity ^0.8.19;
 // private
 // view & pure functions
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
 /*
  * @title DecentralizedStableCoin
  * @author Zurab Anchabadze
@@ -35,6 +38,38 @@ pragma solidity ^0.8.19;
  *
  */
 
-contract DecentralizedStableCoin {
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
 
+    constructor()
+        ERC20("DecentralizedStableCoin", "DSC")
+        Ownable(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266)
+    {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+        super.burn(_amount); // "super" keywork says "use the burn function from a parent class (ERC20Burnable), because we overriding the function
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount); // don't have "super" keyword, because we are not overriding the function
+        return true;
+    }
 }
